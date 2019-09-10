84f8b411132d095a5cf8ca8ced3154129280e1e4
yasu ekimoto <eki.party@gmail.com>
2019-09-10 11:43:52 +0900

    Add a file

--- scripts/wall_stop_accel.py
+++ scripts/wall_stop_accel.py
@@ -23,7 +23,7 @@ class WallStop():
         while not rospy.is_shutdown():
 		data.linear.x += accel
 		
-		if self.sensor_values.sum_all >= 50: data.linear.x = 0
+		if self.sensor_values.sum_all >= 50: data.linear.x = 0.0
 	        elif data.linear.x <= 0.2:           data.linear.x = 0.2
 	        elif data.linear.x >= 0.8:           data.linear.x = 0.8
 
